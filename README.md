e84b7e71970738afde25952590579507c3358bfe
Kanako Kobayashi <mustacheyork@gmail.com>
2015-04-04 07:51:48 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# postSnsApp
+「Swiftビギナーズ勉強会 第６回@Co-Edo」の発表したサンプルです。
+https://swift-beginners.doorkeeper.jp/events/21881
+
+Social.frameworkを利用して、TwitterやFacebookに投稿するアプリです。
+
+＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝ 
+「Swiftビギナーズ勉強会」は、iOSアプリをSwift、Objective-Cなどで開発するための初心者勉強会＆もくもく会です。
+
+開催の連絡は以下の方法で受け取ることができます。 
+
+## DoorKeeperグループ 
+イベント申し込みを受け付けている、DoorKeeper でのグループです。 
+https://swift-beginners.doorkeeper.jp/
+
+## Facebookグループ: 
+イベント告知や技術情報をお互いにシェアしています。 
+https://www.facebook.com/groups/swiftbg/
+
+## Twitterハッシュタグ 
+ハッシュタグは、 #swiftbg です。 
+Twitterにもイベント、勉強会の情報を流しています。 
+
+　　
--- postSnsApp/ViewController.swift
+++ postSnsApp/ViewController.swift
@@ -2,11 +2,12 @@
 //  ViewController.swift
 //  postSnsApp
 //
-//  Created by kobayashikanako on 4/4/15.
+//  Created by Kanako Kobayasi on 4/4/15.
 //  Copyright (c) 2015 com.ticklecode. All rights reserved.
 //
 
 import UIKit
+import Social
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func twitterBtn(sender: AnyObject) {
+        var twitterVC = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+        twitterVC.setInitialText("Twitterに、つぶやくよ")
+        presentViewController(twitterVC, animated: true, completion: nil)
+    }
 
+    @IBAction func facebookBtn(sender: AnyObject) {
+        var facebookVC = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
+        facebookVC.setInitialText("Facebookに、投稿するよ")
+        presentViewController(facebookVC, animated: true, completion: nil)
+    }
 }
 
